26aecf5e4e179d3b41b0536e24c84b3d4a41bf43
hehe <dikka.wangsa@idstar.co.id>
2022-04-07 21:50:20 +0700

    Initial commit, like the wikihow said

---
+++ desktop/src/com/hehe/tiktaktu/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.hehe.tiktaktu.desktop;
+
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.hehe.tiktaktu.Tikitakutu;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Tikitakutu(), config);
+	}
+}
